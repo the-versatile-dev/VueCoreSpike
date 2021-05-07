89cdcd8b129963e1c8b6221e3c69d9a14a3c9631
mike-p <56958874+mike-p@users.noreply.github.com>
2021-05-07 12:17:46 +0100

    Integrated Vue app

---
+++ VueCoreMVCSpikeFive/VueApp/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1>Admin</h1>
+    <admin-nav />
+    
+    <div class="app">
+      <router-view></router-view>
+    </div>
+</div>
+</template>
+<script>
+
+import AdminNav from '@/components/AdminNav';
+
+export default {
+  components: {
+    AdminNav
+  }  
+}
+</script>
+
+<style scoped>
+.app {border: red; }
+</style>
