812bf1857cd55eaaf3d9d0a206f0fbb01043f109
mike-p <56958874+mike-p@users.noreply.github.com>
2021-05-18 12:38:58 +0100

    Spring cleaning for debugging (see Spike docs also)

--- VueCoreMVCSpikeSeven/evo-vue/src/pages/counter/showcounter/showcounter.vue
+++ VueCoreMVCSpikeSeven/evo-vue/src/pages/counter/showcounter/showcounter.vue
@@ -1,9 +1,9 @@
 ﻿<template>
     <div id="showCounter">
         <h2>Show Counter Vue Component</h2>
-        <h3 class="h-40 text-black">The current counter is : {{ $store.state.count }} number?</h3>    
+        <h3 class="h-40 text-black">The current counter is : {{ $store.state.count }} number???</h3>    
         <div>
-            <button type="button" class="btn btn-danger">Primary</button>
+            <button type="button" class="btn btn-outline-success">Primary</button>
         </div>
     </div>   
 </template>
