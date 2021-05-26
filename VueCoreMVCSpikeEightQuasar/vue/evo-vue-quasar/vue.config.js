75c4ba835b297681f7ece4fa9e5d8304fdadadf8
mike-p <56958874+mike-p@users.noreply.github.com>
2021-05-26 15:52:36 +0100

    Added basic-input components to render on quasar razor page

--- VueCoreMVCSpikeEightQuasar/vue/evo-vue-quasar/vue.config.js
+++ VueCoreMVCSpikeEightQuasar/vue/evo-vue-quasar/vue.config.js
@@ -1,11 +1,29 @@
+
+const path = require("path");
+
+
 module.exports = {
-  pluginOptions: {
-    quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: true
-    }
-  },
-  transpileDependencies: [
-    'quasar'
-  ]
+    outputDir: path.resolve(__dirname, "../../wwwroot/dist"),
+    filenameHashing: false,
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        },
+        devtool: 'source-map'
+    },
+    publicPath: '/dist/',
+    pages: {
+        basicinput: {
+            entry: 'src/pages/input/basic-input.js'
+        }
+    },
+    pluginOptions: {
+        quasar: {
+            importStrategy: 'kebab',
+            rtlSupport: true
+        }
+    },
+    transpileDependencies: [
+        'quasar'
+    ]
 }
